dbdb485888a298724e246fb6a1d20f218f956d93
Alexandr <sshnipov@gmail.com>
2021-01-26 23:32:03 +0300

    added js scroll

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
+let burger_menu = document.getElementById('burger_menu')
 
- let burger_menu = document.getElementById('burger_menu')
 function burgerMenu() {
-  
+
   let burger_icon = document.querySelector('.burger_icon')
-   let close_icon = document.getElementById('close_icon')
+  let close_icon = document.getElementById('close_icon')
 
   burger_icon.addEventListener('click', toggleBurgerMenu)
 
-  close_icon.addEventListener('click' , toggleBurgerMenu)
+  close_icon.addEventListener('click', toggleBurgerMenu)
 }
 
-function toggleBurgerMenu(){
+function toggleBurgerMenu() {
   burger_menu.classList.toggle('burger_menu_active')
 }
 
@@ -24,11 +24,30 @@ console.log(buy_buttons)
 let cart_count = document.getElementById('cart_count')
 
 function addToCard() {
-  cart_count.innerText = Number (cart_count.innerText) + 1
+  cart_count.innerText = Number(cart_count.innerText) + 1
 }
 
-for (let i = 0; i < buy_buttons.length; i++){
-  buy_buttons[i].addEventListener('click' , addToCard)
+for (let i = 0; i < buy_buttons.length; i++) {
+  buy_buttons[i].addEventListener('click', addToCard)
 }
 
 new WOW().init();
+
+
+
+let anchors = document.querySelectorAll('a[href*="#"]')
+
+// for (let i = 0; i < anchors.length; i++){
+//   anchors[i]
+// }
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (event) {
+    event.preventDefault()
+    let blockID = anchor.getAttribute('href')
+    document.querySelector(blockID).scrollIntoView({
+      behavior: "smooth",
+      block: 'start'
+    })
+  })
+}
